ef91fe9e7bef4ea0d38ea5a992721ad28f551a88
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2019-08-26 23:05:54 -0700

    first draft

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_may_i_am_a_dreamer_but_i_am_not_the_only_1'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['your-email@example.com']
+	POSTS_PER_PAGE=20
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
